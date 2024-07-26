1395da6dbf6ebe7ab2d6bc110f637e8f9a90c9db
Rodolfo Chacon <rudychacon01@gmail.com>
2024-07-25 22:17:25 -0600

    Add project files.

---
+++ EjercicioHerencia/Operaciones.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace EjercicioHerencia
+{
+    internal class Operaciones
+    {
+        //Area
+        public int a { get; set; }
+        public int b { get; set; } 
+        public int resultado { get; set; }
+
+
+        public void Area()
+        {
+
+            resultado = a * b;
+
+            Console.WriteLine($"El area es: {resultado}");
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
