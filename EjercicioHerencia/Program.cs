6d2553fad1f8a67fe805864b4c58994b9fcc54f1
Rodolfo Chacon <rudychacon01@gmail.com>
2024-07-26 01:14:14 -0600

    avance

--- EjercicioHerencia/Program.cs
+++ EjercicioHerencia/Program.cs
@@ -15,13 +15,61 @@ using System.Dynamic;
 
 
 
-
-
 Aplicacion aplicacion = new Aplicacion();
-aplicacion.CalculoArea();
 
-Aplicacion suma = new Aplicacion();
-aplicacion.CalculoSuma();
 
-Aplicacion resta = new Aplicacion();
-resta.CalculoResta();
+
+
+Console.WriteLine("Seleccione una operación:");
+Console.WriteLine("1. Calcular Área");
+Console.WriteLine("2. Calcular Suma");
+Console.WriteLine("3. Calcular Resta");
+
+int opc = int.Parse(Console.ReadLine());
+
+switch (opc) {
+
+    case 1:
+
+        aplicacion.CalculoArea();
+
+
+        break;
+
+    case 2:
+
+
+        aplicacion.CalculoSuma();
+
+        break;
+
+        case 3:
+
+        aplicacion.CalculoResta();
+
+
+        break;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
